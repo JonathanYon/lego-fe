d1b46098df2565a885d80739a38f32c76e54acc4
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-10 00:27:53 +0100

    navbar modifid and responsive

--- src/components/nav-foot/TopNav.jsx
+++ src/components/nav-foot/TopNav.jsx
@@ -5,7 +5,6 @@ import {
   FormControl,
   Form,
   Button,
-  Dropdown,
 } from "react-bootstrap";
 import { FaCube, FaSearch } from "react-icons/fa";
 import { BsPersonFill } from "react-icons/bs";
@@ -15,93 +14,100 @@ import "./nav-foot.css";
 
 const TopNav = () => {
   return (
-    <>
-      <Container>
-        <Navbar bg="dark" variant="light">
-          <Navbar.Brand href="#home">
+    <Navbar bg="light">
+      <Container className="bg-dark">
+        <Navbar.Brand href="#home">
+          <div className="d-lg-block d-none">
             <img
               src={process.env.PUBLIC_URL + "title.webp"}
               alt=""
               style={{ width: "70%", height: "20%" }}
               className="ml-n5"
             />
-            {/* <img
-            src={process.env.PUBLIC_URL + "title-sm.png"}
-            alt=""
-            style={{ width: "70%", height: "20%" }}
-          /> */}
-          </Navbar.Brand>
-          <Nav
-            className="mr-auto d-flex justify-content-between"
-            style={{ width: "40%" }}
+          </div>
+          <div className="ml-4">
+            <img
+              src={process.env.PUBLIC_URL + "title-sm.png"}
+              alt=""
+              style={{ width: "70%", height: "20%" }}
+              className=" d-lg-none d-sm-block"
+            />
+          </div>
+        </Navbar.Brand>
+        <Nav
+          className="mr-auto d-lg-flex justify-content-between d-none"
+          style={{ width: "40%" }}
+        >
+          <Nav.Link
+            href="#home"
+            className="text-white position-relative nav-item"
           >
-            <Nav.Link
-              href="#home"
-              className="text-white position-relative nav-item"
-            >
-              <div className="d-flex flex-column nav-list-container">
-                <div className="d-flex ">
-                  <FaCube className="nav-list" />
-                  <IoMdArrowDropdown />
-                </div>
-                <small>Sets</small>
+            <div className="d-flex flex-column nav-list-container">
+              <div className="d-flex ">
+                <FaCube className="nav-list" />
+                <IoMdArrowDropdown />
               </div>
-              <div className="drop">
-                <div className="d-flex flex-column text-left ml-2 mt-1">
-                  <small>hello</small>
-                  <small>hello</small>
-                  <small>hello</small>
-                </div>
+              <small>Sets</small>
+            </div>
+            <div className="drop">
+              <div className="d-flex flex-column text-left ml-2 mt-1">
+                <small>hello</small>
+                <small>hello</small>
+                <small>hello</small>
               </div>
-            </Nav.Link>
-            <Nav.Link
-              href="#features"
-              className="text-white position-relative nav-item"
-            >
-              <div className="d-flex flex-column nav-list-container">
-                <div className="d-flex">
-                  <SiBuildkite className="nav-list" />
-                  <IoMdArrowDropdown />
-                </div>
-                <small>Parts</small>
+            </div>
+          </Nav.Link>
+          <Nav.Link
+            href="#features"
+            className="text-white position-relative nav-item"
+          >
+            <div className="d-flex flex-column nav-list-container">
+              <div className="d-flex">
+                <SiBuildkite className="nav-list" />
+                <IoMdArrowDropdown />
               </div>
-              <div className="drop">
-                <div className="d-flex text-left ml-2 flex-column mt-1">
-                  <small>he</small>
-                  <small>he</small>
-                  <small>he</small>
-                </div>
+              <small>Parts</small>
+            </div>
+            <div className="drop">
+              <div className="d-flex text-left ml-2 flex-column mt-1">
+                <small>he</small>
+                <small>he</small>
+                <small>he</small>
               </div>
-            </Nav.Link>
-            <Nav.Link
-              href="#pricing"
-              className="text-white position-relative nav-item"
-            >
-              <div className="d-flex flex-column nav-list-container">
-                <div className="d-flex">
-                  <BsPersonFill className="nav-list" />
-                  <IoMdArrowDropdown />
-                </div>
-                <small>Account</small>
+            </div>
+          </Nav.Link>
+          <Nav.Link
+            href="#pricing"
+            className="text-white position-relative nav-item"
+          >
+            <div className="d-flex flex-column nav-list-container">
+              <div className="d-flex">
+                <BsPersonFill className="nav-list" />
+                <IoMdArrowDropdown />
               </div>
-              <div className="drop">
-                <div className="d-flex text-left ml-2 flex-column mt-1">
-                  <small>helu</small>
-                  <small>helu</small>
-                  <small>helu</small>
-                </div>
+              <small>Account</small>
+            </div>
+            <div className="drop">
+              <div className="d-flex text-left ml-2 flex-column mt-1">
+                <small>helu</small>
+                <small>helu</small>
+                <small>helu</small>
               </div>
-            </Nav.Link>
-          </Nav>
-          <Form inline className="mr-5 d-flex">
-            <FormControl type="text" placeholder="Search" className="" />
-            <Button variant="success">
-              <FaSearch />
-            </Button>
-          </Form>
-        </Navbar>
+            </div>
+          </Nav.Link>
+        </Nav>
+        <Form inline className="mr-5 d-lg-flex d-none">
+          <FormControl type="text" placeholder="Search" className="" />
+          <Button variant="success">
+            <FaSearch />
+          </Button>
+        </Form>
+        <div className="d-lg-none d-flex flex-column text-white">
+          <FaSearch className="search" />
+          <small className="mr-3">Search</small>
+        </div>
       </Container>
-    </>
+    </Navbar>
   );
 };
 export default TopNav;
