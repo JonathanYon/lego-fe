cb48bf6fa813aaf2918c2a8f7c970f4e597655e6
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-10 10:43:49 +0100

    footer + photo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Footers from "./components/nav-foot/Footers";
 import TopNav from "./components/nav-foot/TopNav";
 
 function App() {
   return (
     <div className="App">
       <TopNav />
+      <Footers />
     </div>
   );
 }
---
+++ src/components/nav-foot/Footers.jsx
@@ -0,0 +1,8 @@
+const Footers = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+export default Footers;
