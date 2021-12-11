e346bd6dce207f0683e44e078ca922d8869fd659
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-11 19:52:58 +0100

    sets.js reducer

---
+++ src/redux/reducers/sets.js
@@ -0,0 +1,24 @@
+import { SETS, ERROR_SETS, LOADING_SETS } from "../action";
+import { initialState } from "../store";
+
+export const setsReducer = (state = initialState.sets, action) => {
+  switch (action.type) {
+    case SETS:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case ERROR_SETS:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case LOADING_SETS:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    default:
+      return state;
+  }
+};
