3f69803e818c8e635efa69c2bef322aab5764a63
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-11 19:51:17 +0100

    MiniFig component

--- src/components/home/MiniFigs.jsx
+++ src/components/home/MiniFigs.jsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { getMiniFigs } from "../../redux/action";
+import SingleMini from "./SingleMini";
 
 const MiniFigs = () => {
+  const minifigs = useSelector((state) => state.minifigs.data);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -10,9 +14,15 @@ const MiniFigs = () => {
   }, []);
 
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Container>
+      <Row sm={2} md={4}>
+        {/* <Col className=""> */}
+        {minifigs.map((mini, i) => (
+          <SingleMini key={i + 1} minifig={mini} />
+        ))}
+        {/* </Col> */}
+      </Row>
+    </Container>
   );
 };
 export default MiniFigs;
