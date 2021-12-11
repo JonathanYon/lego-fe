317a253f9f5c0e8b8a5b0e53a62a1b88fdfd6a9a
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-11 19:52:41 +0100

    parts.js reducer

---
+++ src/redux/reducers/parts.js
@@ -0,0 +1,24 @@
+import { PARTS, ERROR_PARTS, LOADING_PARTS } from "../action";
+import { initialState } from "../store";
+
+export const partsReducer = (state = initialState.parts, action) => {
+  switch (action.type) {
+    case PARTS:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    case ERROR_PARTS:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case LOADING_PARTS:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    default:
+      return state;
+  }
+};
