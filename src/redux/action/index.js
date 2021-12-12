bb71e257520ff37c99bfceb32a5e4d218f67a199
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-12 22:14:52 +0100

    getSets

--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -26,7 +26,7 @@ export const getMiniFigs = () => {
         });
         dispatch({
           type: MINI,
-          payload: res.results.slice(10, 22),
+          payload: res.results,
         });
       } else {
         dispatch({
@@ -46,6 +46,60 @@ export const getMiniFigs = () => {
       console.log(error);
       dispatch({
         type: ERROR_MINI,
+        payload: true,
+      });
+    }
+  };
+};
+
+// Sets
+
+export const getSets = (search, minYear, maxYear, minPart, maxPart) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_URL}/sets?page=2${
+          search && "&search=" + search
+        }${minYear && "&min_year=" + minYear}${
+          maxYear && "&max_year=" + maxYear
+        }${minPart && "&min_parts=" + minPart}${
+          maxPart && "&max_parts=" + maxPart
+        }`,
+        {
+          headers: {
+            Authorization: `Key ${process.env.REACT_APP_KEY}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const res = await response.json();
+        console.log("sets", res.results.slice(0, 12));
+        dispatch({
+          type: LOADING_SETS,
+          payload: false,
+        });
+        dispatch({
+          type: SETS,
+          payload: res.results,
+        });
+      } else {
+        dispatch({
+          type: LOADING_SETS,
+          payload: false,
+        });
+        dispatch({
+          type: ERROR_SETS,
+          payload: true,
+        });
+      }
+    } catch (error) {
+      dispatch({
+        type: LOADING_SETS,
+        payload: false,
+      });
+      console.log(error);
+      dispatch({
+        type: ERROR_SETS,
         payload: true,
       });
     }
