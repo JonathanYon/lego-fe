467d70216d84f9d5769734e9233a660903c1cc4d
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-13 15:49:59 +0100

    like fav sets

--- src/components/home/SetDetail.jsx
+++ src/components/home/SetDetail.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import { Col, Container, Row, ListGroup } from "react-bootstrap";
+import { AiOutlineHeart, AiFillHeart, AiFillStar } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleFav } from "../../redux/action";
+import "./home.css";
 
 const SetDetail = ({ match }) => {
   const [detail, setDetail] = useState(null);
+
+  const like = useSelector((state) => state.fav.liked);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getSetsDetail = async () => {
@@ -29,12 +36,28 @@ const SetDetail = ({ match }) => {
     };
     getSetsDetail();
   }, []);
-  console.log("det", detail);
+  console.log("det", like);
   return (
     <Container className="my-5">
       <Row>
-        <Col sm={8}>
+        <Col sm={8} className="d-flex flex-column">
           <img src={detail?.set_img_url} alt="" />
+          <div className="bg-warning">
+            {!like.includes(detail) ? (
+              <AiOutlineHeart
+                className="heart"
+                onClick={() => dispatch(toggleFav(detail))}
+              />
+            ) : (
+              <AiFillHeart
+                className="heart"
+                onClick={() => dispatch(toggleFav(detail))}
+              />
+            )}
+          </div>
+          <div className="bg-success mt-2">
+            <AiFillStar /> <strong>My Favourite Sets</strong>
+          </div>
         </Col>
         <Col sm={4} className="bg-light mt-1">
           <small>Login to hide the ads</small>
