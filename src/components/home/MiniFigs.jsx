7f5ee430e113635c00b069cf8905d86ef72c7aed
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-12-14 11:50:17 +0100

    loader + error card for minifig

--- src/components/home/MiniFigs.jsx
+++ src/components/home/MiniFigs.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, ListGroup, Spinner } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { getMiniFigs } from "../../redux/action";
@@ -7,6 +7,8 @@ import SingleSet from "./SingleSet";
 
 const MiniFigs = () => {
   const minifigs = useSelector((state) => state.minifigs.data);
+  const errors = useSelector((state) => state.minifigs.error);
+  const loading = useSelector((state) => state.minifigs.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,6 +18,20 @@ const MiniFigs = () => {
   return (
     <Container>
       <Row sm={2} md={4}>
+        {errors && (
+          <ListGroup className="mt-3 mb-5 mx-auto">
+            <ListGroup.Item variant="danger">
+              <strong>
+                Something has gone wrong please come back again later
+              </strong>
+            </ListGroup.Item>
+          </ListGroup>
+        )}
+        {loading && (
+          <div className="mx-auto my-5">
+            <Spinner animation="grow" className="" />
+          </div>
+        )}
         {/* <Col className=""> */}
         {minifigs
           .map((data) => <SingleSet key={data.set_num} data={data} />)
